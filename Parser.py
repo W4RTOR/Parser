7050fe82ec37bd4bfddd5d1490d8520ef2b18f12
W4RTOR <39349676+W4RTOR@users.noreply.github.com>
2023-02-14 23:39:11 +0300

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -5,11 +5,11 @@ url = 'https://policies.google.com/'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 print(soup)
-print("1")
+
 # Вы можете искать конкретные теги и атрибуты
 title = soup.find('title').text
 print(title)
-print("2")
+
 # Или искать все элементы с определенным классом
 all_paragraphs = soup.find_all('p', class_='text-class')
 for p in all_paragraphs:
